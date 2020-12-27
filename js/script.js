38d17ef3c59f54f206b1a9bbb570d73f5f3882f3
John Delizo <johndelizo99@gmail.com>
2020-12-26 22:10:34 -0800

    displays and clears questions

--- js/script.js
+++ js/script.js
@@ -32,8 +32,27 @@ function startQuiz() {
   function getQuestion() {
     // get current question object from array
     var currentQuestion = questions[currentQuestionIndex];
-  }
-
+  
     // update title with current question
     var titleEl = document.getElementById("question-title");
     titleEl.textContent = currentQuestion.title;
+  
+    // clear out any old question choices
+    choicesEl.innerHTML = "";
+  
+    // loop over choices
+    currentQuestion.choices.forEach(function(choice, i) {
+      // create new button for each choice
+      var choiceNode = document.createElement("button");
+      choiceNode.setAttribute("class", "choice");
+      choiceNode.setAttribute("value", choice);
+  
+      choiceNode.textContent = i + 1 + ". " + choice;
+  
+      // attach click event listener to each choice
+      choiceNode.onclick = questionClick;
+  
+      // display on the page
+      choicesEl.appendChild(choiceNode);
+    });
+  }
