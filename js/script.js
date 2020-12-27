354ce4503db6aa59109e948814705b7c1ad6b6af
John Delizo <johndelizo99@gmail.com>
2020-12-27 12:38:34 -0800

    saves users high score along with their initials to local storage

--- js/script.js
+++ js/script.js
@@ -118,3 +118,28 @@ function startQuiz() {
       quizEnd();
     }
   }
+
+  function saveHighscore() {
+    // get value of input box
+    var initials = initialsEl.value.trim();
+  
+    // make sure value wasn't empty
+    if (initials !== "") {
+      // get saved scores from localstorage, or if not any, set to empty array
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+      // format new score object for current user
+      var newScore = {
+        score: time,
+        initials: initials
+      };
+  
+      // save to localstorage
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+  
+      // redirect to next page
+      window.location.href = "highscores.html";
+    }
+  }
